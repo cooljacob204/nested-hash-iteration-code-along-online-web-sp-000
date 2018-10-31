2879dba604bff1ff54803567b295b1890944fac1
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-31 16:21:45 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,9 +16,9 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |name|
-    name.favorite_ice_cream_flavors.each_with_index do |i, index|
+    name[favorite_ice_cream_flavors]each_with_index do |i, index|
       if i == "strawberry"
-        name.favorite_ice_cream_flavors[index].delete
+        name[favorite_ice_cream_flavors][index].delete
       end
     end
   end
